544e224c5d0368a6914691c3c218dbe27734fd87
Polona Caserman <polona.caserman@kom.tu-darmstadt.de>
2021-08-10 16:45:35 +0200

    Stroke the avatar smoothly

--- Assets/Paintbrush/StrokePlayer.cs
+++ Assets/Paintbrush/StrokePlayer.cs
@@ -6,53 +6,52 @@ public class StrokePlayer : MonoBehaviour
 {
 
     public GameObject[] pathNode;
-    private GameObject paintbrush;
-    private const float moveSpeed = 1;
-    float timer;
-    int currentNode;
+    private const float moveSpeed = 0.5f;
+    private int currentNode;
+    private int step;
+    private float timer;
+
     private Vector3 startPosition;
-    static Vector3 currentPositionHolder;
+    private Quaternion startRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        paintbrush = this.gameObject;
-
-        currentNode = 0;
-        startPosition = pathNode[currentNode].transform.position;
-        currentPositionHolder = pathNode[currentNode].transform.position;
-
         timer = 0;
+        currentNode = 0;
+        step = 1;
+
+        startPosition = pathNode[currentNode].transform.position;
+        startRotation = pathNode[currentNode].transform.rotation;
+        transform.position = startPosition;
+        transform.rotation = startRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         timer += Time.deltaTime * moveSpeed;
 
-        if (paintbrush.transform.position != currentPositionHolder)
+        if (transform.position != pathNode[currentNode].transform.position)
         {
-            paintbrush.transform.position = Vector3.Lerp(startPosition, currentPositionHolder, timer);
+            // Move the paintbrush towards the second node
+            transform.position = Vector3.Lerp(startPosition, pathNode[currentNode].transform.position, timer);
+
+            // Rotate the paintbrush towards the second node (y-axis)
+            transform.rotation = Quaternion.Lerp(startRotation, pathNode[currentNode].transform.rotation, timer);
         }
         else
         {
-            if (currentNode >= 0 && currentNode < pathNode.Length - 1)
-            {
-                currentNode++;
-            }
-            else if (currentNode >= pathNode.Length - 1)
-            {
-                currentNode--;
-            }
-            checkNode();
-        }
-    }
+            if (currentNode <= 0) step = 1;
+            else if (currentNode >= pathNode.Length - 1) step = -1;
 
-    void checkNode()
-    {
-        timer = 0;
-        startPosition = paintbrush.transform.position;
-        currentPositionHolder = pathNode[currentNode].transform.position;
+            currentNode += step;
+            timer = 0;
+            
+            startPosition = transform.position;
+            startRotation = transform.rotation;
+        }
     }
 
 }
