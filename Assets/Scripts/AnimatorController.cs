0b722fa52aa0f2f7f502207a1386c6e5a513b585
Polona Caserman <polona.caserman@kom.tu-darmstadt.de>
2021-10-13 13:25:54 +0200

    Add idle standing animation

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -6,20 +6,23 @@ public class AnimatorController : MonoBehaviour
 {
 
     private Animator anim;
+    private GameObject knife;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        knife = GameObject.FindGameObjectWithTag("Knife");
+        knife.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Standing Idle") && Input.GetKeyDown(KeyCode.Space))
         {
+            knife.SetActive(true);
             anim.SetTrigger("Stab");
         }
-
     }
 }
